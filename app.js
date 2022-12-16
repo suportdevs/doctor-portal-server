f846440d062bce8563424a6b6c4870281a045876
suportdevs <suportdevs@gmail.com>
2022-12-16 22:34:12 +0600

    created app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // external imports
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotEnv = require('dotenv');
@@ -8,3 +9,13 @@ const dotEnv = require('dotenv');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
+dotEnv.config();
+
+mongoose.connect(process.env.DATABASE_CONNECTION_URI, {useNewUrlParser: true})
+.then(() => {console.log('Database connection successfull.')})
+.catch((err) => { console.log(err)});
+
+app.listen(process.env.PORT, (req, res) =>{
+    console.log(`Server is runging  on port ${process.env.PORT}`);
+})
