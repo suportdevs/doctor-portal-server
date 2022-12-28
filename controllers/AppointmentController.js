363a4bd247bcca78ebd85a469fc838719a924652
suportdevs <suportdevs@gmail.com>
2022-12-28 23:06:43 +0600

    working on others project

--- controllers/AppointmentController.js
+++ controllers/AppointmentController.js
@@ -2,7 +2,6 @@ const Appointment = require("../models/Appointment");
 
 async function storeAppointment(req, res){
     try{
-        console.log(req.body);
         const appointment = await new Appointment(req.body);
         await appointment.save();
         res.status(200).json({message: 'sucess'})
